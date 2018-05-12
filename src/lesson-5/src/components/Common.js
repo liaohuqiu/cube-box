3aa66dbd1e4ee508ed506aace6de0acc37743589
jason0539 <jason0539@163.com>
2018-05-12 17:49:20 +0800

    去掉未完成的前端接收事件通知的代码

--- src/lesson-5/src/components/Common.js
+++ src/lesson-5/src/components/Common.js
@@ -10,27 +10,12 @@ class Common extends Component {
 
   componentDidMount() {
     const { payroll, web3 } = this.props;
-    const updateInfo = (error, result) => {
-      if (!error) {
-        this.checkInfo();
-      }
-    }
-
-    this.newFund = payroll.NewFund(updateInfo);
-    this.getPaid = payroll.GetPaid(updateInfo);
-    this.newEmployee = payroll.NewEmployee(updateInfo);
-    this.updateEmployee = payroll.UpdateEmployee(updateInfo);
-    this.removeEmployee = payroll.RemoveEmployee(updateInfo);
 
     this.checkInfo();
   }
 
   componentWillUnmount() {
-    this.newFund.stopWatching();
-    this.getPaid.stopWatching();
-    this.newEmployee.stopWatching();
-    this.updateEmployee.stopWatching();
-    this.removeEmployee.stopWatching();
+    
   }
 
   checkInfo = () => {
